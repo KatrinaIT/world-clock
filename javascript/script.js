08713b18cc298fbb7ddd6e96e7288ef677d2e2cb
KatrinaIT <79994321+KatrinaIT@users.noreply.github.com>
2024-05-24 22:55:06 -0600

    updated city date/time with moment.js timezone

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,24 @@
-//alert("Hi");
+function updateTime() {
+  //LA
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDate = losAngelesElement.querySelector(".date");
+  let losAngelesTime = losAngelesElement.querySelector(".time");
+  let losAngelesMoment = moment().tz("America/Los_Angeles");
+
+  losAngelesDate.innerHTML = losAngelesMoment.format("MMMM Do, YYYY");
+  losAngelesTime.innerHTML = losAngelesMoment.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDate = tokyoElement.querySelector(".date");
+  let tokyoTime = tokyoElement.querySelector(".time");
+  let tokyoMoment = moment().tz("Asia/Tokyo");
+
+  tokyoDate.innerHTML = tokyoMoment.format("MMMM Do, YYYY");
+  tokyoTime.innerHTML = tokyoMoment.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
